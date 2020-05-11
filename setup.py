b161a61e24a6cc7f8a933b0c5e3968f431c3e8f1
Yuri D'Elia <wavexx@thregr.org>
2020-05-11 14:37:25 +0200

    Re-introduce dependency on distutils-extra for i18n
    This was/is required to build/install localization

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setup(name='screenkey', version='0.9',
 
       scripts=['screenkey'],
       packages=['Screenkey'],
-      setup_requires=['setuptools'],
+      setup_requires=['setuptools', 'python-distutils-extra'],
       install_requires=['PyGObject', 'pycairo'],
       data_files=[('share/applications', ['data/screenkey.desktop']),
                   ('share/doc/screenkey', ['README.rst', 'NEWS.rst'])],
